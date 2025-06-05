1a7c577a8f0bac731f11778de4f218b02fab8a13
Usmantahir0707 <134065023+Usmantahir0707@users.noreply.github.com>
2025-06-05 08:36:43 +0500

    Update script.js

--- script.js
+++ script.js
@@ -58,11 +58,12 @@ const generateCards = (arr) => {
   if (!cardsFlex) return;
   cardsFlex.innerHTML = arr
     .map((x) => {
+      let pop = x.population.toLocalString();
       return `<div class="c-box">
         <img class="c-image" src=${x.flags.png} alt="">
         <div class="c-title">${x.name.common}</div>
         <div class="c-details">
-            <p>Population: ${x.population.toLocalString()}</p>
+            <p>Population: ${pop}</p>
             <p>Region: ${x.region}</p>
             <p>Capital: ${x.capital}</p>
         </div>
